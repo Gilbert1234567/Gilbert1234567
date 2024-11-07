1222f1c3c30ed61632c9b8a3b3cc2bd71351af69
Gilbert1234567 <gilbertkipkech2019@gmail.com>
2024-11-07 18:57:55 +0530

    Add files via upload

---
+++ 07.11.2024/fileandstructures.c
@@ -0,0 +1,30 @@
+/*using structures and files  */
+/* Gilbert Kipkech*/
+/* Roll No. 33*/
+/* Date 05.11.2024*/
+
+#include <stdio.h>
+	  	main() 
+		
+		  {
+	  		struct emp{
+    FILE *fptr;
+    char name[25];
+    int eid;
+}x[5];
+    x[0].fptr = fopen("Employee.DAT", "w");
+        printf("Enter the Employee Details.\n");
+	
+	for(int i=0;i<=5;i++)
+	{
+    printf("employee ID: ");
+    scanf("%d", &x[i].eid);
+
+    printf("Enter Employee name: ");
+    scanf("%s", x[i].name);
+
+    fprintf(x[i].fptr,"%s %d\n",&x[i].eid, x[i].name);
+
+    fclose(x[i].fptr); 
+}
+}
